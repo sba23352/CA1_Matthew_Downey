5773c69c8508e5813c9dffb6337b4a222924c253
sba23352 <sba23352@student.cct.ie>
2024-01-31 14:53:47 +0000

    Initial commit

---
+++ src/ca1_matthew_downey/CA1_Matthew_Downey.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1_matthew_downey;
+
+/**
+ *
+ * @author amber
+ */
+public class CA1_Matthew_Downey {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
